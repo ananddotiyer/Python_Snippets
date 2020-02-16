a0d12e09eb61795f9947402ef1c3ad1f274b3384
Anand Iyer <ananddotiyer@gmail.com>
2020-02-16 21:08:37 +0530

    INitial

---
+++ Python classes.cmd
@@ -0,0 +1,1 @@
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'D:\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'D:\Anaconda3' ";D:\Anaconda3\scripts\jupyter-notebook
